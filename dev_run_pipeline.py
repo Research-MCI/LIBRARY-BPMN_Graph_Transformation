7bd2cdc9048e22d11d6b6b1c663a1fcef64a0e5e
IzzatAji1 <143975925+IzzatAji1@users.noreply.github.com>
2025-08-04 12:46:39 +0700

    chore: upload new library structure

--- dev_run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from app.core.libraries.graph_converter import BPMNGraphConverter
-
-converter = BPMNGraphConverter(config={
-    "neo4j_uri": "bolt://localhost:7687",
-    "neo4j_user": "neo4j",
-    "neo4j_password": "12345678"
-})
-result = converter.convert_file("data/bpmn_parse_output_20250602_164152.json", save_cypher_to="output/output.cypher")
-print(result)
